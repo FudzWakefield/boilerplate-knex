04b77ab80b9527435800b170e5d3a626451a5497
reidvr <reidvr@gmail.com>
2019-04-03 17:52:08 +1300

    Create Add user profile functionality

--- db.js
+++ db.js
@@ -5,25 +5,47 @@ const connection = require('knex')(config)
 module.exports = {
   getUser,
   getUsers,
-  getProfile
-}
-
-function getUsers (db = connection) {
-  return db('users').select()
+  updateUserProfileID,
+  addUser,
+  getProfile,
+  addProfile
 }
 
 function getUser (id, db = connection) {
   return db('users').where('id', id).first()
 }
 
+function getUsers (db = connection) {
+  return db('users').select()
+}
+
+function updateUserProfileID (userID, profileID, db = connection) {
+  return db('users')
+  .where('id', userID)
+  .update( 'profile_id', profileID )
+}
+
+function addUser(newUser, db = connection) {
+  return db('users')
+   .insert(newUser)
+ }
+
 function getProfile(userID, db = connection) {
-  console.log('userID: ', userID);
-  
   return db('users')
   .join('profiles', 'user_id', '=', 'users.id')
   .where({'users.id': userID})
   .select()
   .first()
 }
+
+
+
+
+function addProfile(newProfile, db = connection) {
+  return db('profiles')
+   .insert(newProfile)
+   
+ }
+
 
 //'*', 'users.id AS user_id', 'profiles.id AS profile_id'
--- routes/users.js
+++ routes/users.js
@@ -28,5 +28,49 @@ router.get('/profile/:id', (req, res) => {
     })
 })
 
+router.get('/adduser', (req, res) => {
+  res.render('partials/adduser')
+})
 
+
+router.post('/adduser', async (req, res) => {
+  try{
+    let newUserProfile = req.body
+
+    let newUser = {
+      name: newUserProfile.name,
+      email: newUserProfile.email
+    }
+
+    let userID = await db.addUser(newUser);
+    userID = userID[0]
+
+    let newProfile = {
+      website_URL: newUserProfile.website_URL,
+      profile_picture: newUserProfile.profile_pic,
+      user_id: userID
+    }
+
+    
+    let profileID = await db.addProfile(newProfile);
+    profileID = profileID[0]
+
+    await db.updateUserProfileID(userID, profileID);
+
+    res.redirect('/');
+}
+catch(err){
+  console.log('err: ', err);
+
+}
+  
+  
+})
 module.exports = router
+
+
+// { website_URL: 'www.gandalf.com',
+//   profile_picture:
+//    'https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Gandalf600ppx.jpg/220px-Gandalf600ppx.jpg',
+//   user_id: [ 99937 ] 
+// }
